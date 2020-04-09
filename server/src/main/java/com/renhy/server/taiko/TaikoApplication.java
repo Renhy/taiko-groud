4ee52f03250d115175f4f6cd2a991aacfab26fc1
Renhy <592672378@qq.com>
2020-04-09 09:39:10 +0800

    add server

---
+++ server/src/main/java/com/renhy/server/taiko/TaikoApplication.java
@@ -0,0 +1,13 @@
+package com.renhy.server.taiko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaikoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaikoApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/renhy/server/taiko/TaikoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renhy.server.taiko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaikoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
