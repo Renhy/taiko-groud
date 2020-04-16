f19e56b48bfa77b34d733eb9c1619e63cfcdf727
Renhy <592672378@qq.com>
2020-04-16 11:48:46 +0800

    update background color

--- web/src/js/game/view/back.js
+++ web/src/js/game/view/back.js
@@ -4,14 +4,14 @@ export class Back {
     async init(game) {
         this.game = game;
 
-        this.middle = document.getElementById('background-middle');
-        await new Promise((resolve, reject) => {
-            this.middle.onload = () => {
-                return resolve();
-            };
+        // this.middle = document.getElementById('background-middle');
+        // await new Promise((resolve, reject) => {
+        //     this.middle.onload = () => {
+        //         return resolve();
+        //     };
 
-            this.middle.src = '/assets/img/background-1.png';
-        });
+        //     this.middle.src = '/assets/img/background-1.png';
+        // });
 
         this.gaugebuttom = document.getElementById('game-gauge-bottom');
         await new Promise((resolve, reject) => {
