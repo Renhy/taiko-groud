51b807344d7dbfc384d38b4c50f50722b25785ba
Renhy <592672378@qq.com>
2020-04-16 15:16:34 +0800

    update drum beat render

--- web/src/js/game/view/beat.js
+++ web/src/js/game/view/beat.js
@@ -7,11 +7,11 @@ var Layout = {
         height: 128 / 720,
     },
     drum: {
-        last: 150,
-        left: 512 / 1280,
-        top: 470 / 720,
-        width: 256 / 1280,
-        height: 256 / 720,
+        last: 100,
+        left: 400 / 1280,
+        top: 312 / 720,
+        width: 480 / 1280,
+        height: 480 / 720,
     }
 };
 
@@ -21,10 +21,11 @@ export class Beat {
         this.sticker = game.plotter.sticker;
         this.referee = game.referee;
 
-        await this.sticker.loadTexture('leftDo', '/assets/img/left-do.png');
-        await this.sticker.loadTexture('rightDo', '/assets/img/right-do.png');
-        await this.sticker.loadTexture('leftKa', '/assets/img/left-ka.png');
-        await this.sticker.loadTexture('rightKa', '/assets/img/right-ka.png');
+        await this.sticker.loadTexture('drum', '/assets/img/drum.png');
+        await this.sticker.loadTexture('leftDo', '/assets/img/drum-leftDo.png');
+        await this.sticker.loadTexture('rightDo', '/assets/img/drum-rightDo.png');
+        await this.sticker.loadTexture('leftKa', '/assets/img/drum-leftKa.png');
+        await this.sticker.loadTexture('rightKa', '/assets/img/drum-rightKa.png');
         await this.sticker.loadTexture('beat-start', '/assets/img/beat-point-start.png');
         await this.sticker.loadTexture('beat-ggt', '/assets/img/beat-point-ggt.png');
     }
@@ -37,6 +38,10 @@ export class Beat {
             this.sticker.stick('beat-start', 
             Layout.beat.left, Layout.beat.top, Layout.beat.width, Layout.beat.height);
         }
+
+
+        this.sticker.stick('drum',
+            Layout.drum.left, Layout.drum.top, Layout.drum.width, Layout.drum.height);
 
         if (delta - this.referee.state.play.leftDo < Layout.drum.last) {
             this.sticker.stick('leftDo', 
