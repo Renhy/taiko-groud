5ac3ff3dea7d54c88c80a44dd4855f0f15ac5a6f
Renhy <592672378@qq.com>
2020-04-29 14:31:29 +0800

    add h2database for embedded dataSource in application test

--- server/src/test/java/com/renhy/server/taiko/TaikoApplicationTests.java
+++ server/src/test/java/com/renhy/server/taiko/TaikoApplicationTests.java
@@ -2,8 +2,14 @@ package com.renhy.server.taiko;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaikoApplicationTests {
 
     @Test
